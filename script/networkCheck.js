98581fecc0e480b29e23c73083268f7edec68fde
Sullivan Suen <51816299+hsshhsxm@users.noreply.github.com>
2022-02-04 16:52:04 +0800

    Update networkCheck.js

--- script/networkCheck.js
+++ script/networkCheck.js
@@ -47,7 +47,7 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
     if (error) {
       $done({
         title: '发生错误',
-        content: '无法获得目前网络信息\n请检查网络状态后重试',
+        content: '无法获得当前网络信息\n请检查网络状态后重试',
         icon: 'wifi.exclamationmark',
         'icon-color': '#CB1B45',
       });
